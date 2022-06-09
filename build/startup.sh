7d6fefa5e0b43080d6d2c0dd8189e072b4bd2991
Davide Motte <37443982+dmotte@users.noreply.github.com>
2022-06-09 19:35:20 +0200

    Simplified startup.sh

--- build/startup.sh
+++ build/startup.sh
@@ -46,25 +46,18 @@ rm -f /tmp/.X0-lock
 
 echo "Starting Xvfb"
 /usr/bin/Xvfb :0 -screen 0 "${RESOLUTION}x24" -nolisten tcp -nolisten unix &
-PID_XVFB=$!
 sleep 2
 
 echo "Starting LXDE"
 DISPLAY=:0 /usr/bin/startlxde &
-PID_LXDE=$!
 sleep 2
 
 echo "Starting x11vnc"
 /usr/bin/x11vnc -display :0 -xkb -forever -shared -repeat -capslock \
     -rfbport "$VNC_PORT" "$VNCPWOPTION" &
-PID_X11VNC=$!
 sleep 2
 
 echo "Starting noVNC"
 /usr/bin/websockify --web=/usr/share/novnc "$NOVNC_PORT" "127.0.0.1:$VNC_PORT" &
-PID_NOVNC=$!
 
-wait "$PID_XVFB"
-wait "$PID_LXDE"
-wait "$PID_X11VNC"
-wait "$PID_NOVNC"
+wait # until all jobs finish
